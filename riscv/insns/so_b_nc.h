f4257c37b690849efd3bcdcd36ceaaba9786949c
chocoflake <abeatriz.sfernandes@gmail.com>
2024-11-10 23:41:51 +0000

    cleanup so_b_nc

--- riscv/insns/so_b_nc.h
+++ riscv/insns/so_b_nc.h
@@ -5,13 +5,5 @@ const bool notComplete = std::visit([](const auto &reg){
     return !reg.hasStreamFinished();
 }, streamReg);
 
-/*auto streamReg = insn.uve_branch_rs();
-auto branchIMM = insn.uve_branch_imm();
-
-const bool notComplete = operateRegister(P.SU, streamReg, [=](auto& reg) {
-    return !reg.hasStreamFinished();
-});
-*/
-
 if (notComplete)
     set_pc(pc + branchIMM);
