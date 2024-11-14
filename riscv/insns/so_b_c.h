098371490cd03af4180a77edf8cbd12350fcf19a
chocoflake <abeatriz.sfernandes@gmail.com>
2024-11-14 17:53:08 +0000

    update encoding to reflect new dimension numbering

--- riscv/insns/so_b_c.h
+++ riscv/insns/so_b_c.h
@@ -5,13 +5,5 @@ const bool complete = std::visit([](const auto &reg){
     return reg.hasStreamFinished();
 }, streamReg);
 
-/*auto streamReg = insn.uve_branch_rs();
-auto branchIMM = insn.uve_branch_imm();
-
-const bool notComplete = operateRegister(P.SU, streamReg, [=](auto& reg) {
-    return !reg.hasStreamFinished();
-});
-*/
-
 if (complete)
     set_pc(pc + branchIMM);
--- riscv/insns/so_b_dc_8.h
+++ riscv/insns/so_b_dc_8.h
@@ -1,5 +1,5 @@
 auto regN = insn.uve_branch_rs();
 auto branchIMM = insn.uve_branch_imm();
 
-if (P.SU.EODTable.at(regN).at(0))
+if (P.SU.EODTable.at(regN).at(7))
     set_pc(pc + branchIMM);
